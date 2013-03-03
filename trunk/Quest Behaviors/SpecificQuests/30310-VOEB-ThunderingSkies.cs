fa9707e65dd760a7867b3ae6541519d67eb00d73
brodiebruce08@gmail.com <brodiebruce08@gmail.com@72654b22-5f91-dfbe-c595-3f911aafe2ed>
2013-03-03 01:24:39 +0000

    r85 Quest Behaviors -- Added replacement behavior for Thundering Skies. NOT TESTED - ONLY RUN/REPLACE IF YOU PLAN TO REPORT ON IT!!! Pandaria Dailies v2 -- Raised to v2.1.1 -- Added support for Shredmaster Packle spell avoidance (Boot to the Head) -- Added support for Ace Longpaw ground aoe avoidance (Hot Foot!) -- Added support for Suchi the Sweet ground aoe avoidance (Solar Beam) Cloud Serpent -- Code Cleanup -- Disabled Sky Race Warning. Will now run regardless The Tillers -- PreReq check disabled temporarily -- Weed War II - Converted macro to gossip option to InteractWith with gossip options Red Crane Temple -- Ellia Ravenmane: Redemption - Fixed flyto location -- Code Cleanup Whitepetal Lake -- Code Cleanup -- Fiery Tongue, Fragile Feet - Converted Objective to CTM InteractWith, needs full testing -- Sparkle in the Eye - Removed Redundant Objective code

---
+++ trunk/Quest Behaviors/SpecificQuests/30310-VOEB-ThunderingSkies.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using CommonBehaviors.Actions;
+using Styx;
+using Styx.Common;
+using Styx.CommonBot;
+using Styx.CommonBot.Profiles;
+using Styx.Pathing;
+using Styx.TreeSharp;
+using Styx.WoWInternals;
+using Styx.WoWInternals.WoWObjects;
+using Action = Styx.TreeSharp.Action;
+
+namespace ThunderingSkies
+{
+    public class ThunderingSkies : CustomForcedBehavior
+    {
+        public ThunderingSkies(Dictionary<string, string> args)
+            : base(args)
+        {
+            try
+            {
+                QuestId = 30310;//GetAttributeAsQuestId("QuestId", true, null) ?? 0;
+                SpellIds = GetNumberedAttributesAsArray<int>("SpellId", 1, ConstrainAs.SpellId, null);
+                //SpellId = GetAttributeAsNullable<int>("SpellId", false, ConstrainAs.SpellId, null) ?? 0;
+                SpellId = SpellIds.FirstOrDefault(id => SpellManager.HasSpell(id));
+            }
+            catch
+            {
+                Logging.Write("Problem parsing a QuestId in behavior: Thundering Skies");
+            }
+        }
+        public int QuestId { get; set; }
+        private bool _isBehaviorDone;
+        public int MobIdSerpent = 59158;
+        public int[] SpellIds { get; private set; }
+        public int SpellId { get; private set; }
+        private Composite _root;
+        public QuestCompleteRequirement questCompleteRequirement = QuestCompleteRequirement.NotComplete;
+        public QuestInLogRequirement questInLogRequirement = QuestInLogRequirement.InLog;
+		static public bool InVehicle { get { return Lua.GetReturnVal<int>("if IsPossessBarVisible() or UnitInVehicle('player') or not(GetBonusBarOffset()==0) then return 1 else return 0 end", 0) == 1; } }
+        public override bool IsDone
+        {
+            get
+            {
+                return _isBehaviorDone;
+            }
+        }
+        private LocalPlayer Me
+        {
+            get { return (StyxWoW.Me); }
+        }
+
+        public override void OnStart()
+        {
+            OnStart_HandleAttributeProblem();
+            if (!IsDone)
+            {
+                PlayerQuest Quest = StyxWoW.Me.QuestLog.GetQuestById((uint)QuestId);
+                TreeRoot.GoalText = ((Quest != null) ? ("\"" + Quest.Name + "\"") : "In Progress");
+            }
+        }
+
+        public WoWSpell CurrentBehaviorSpell
+        {
+            get
+            {
+                return WoWSpell.FromId(SpellId);
+            }
+        }
+
+        public List<WoWUnit> Serpent
+        {
+            get
+            {
+                return ObjectManager.GetObjectsOfType<WoWUnit>().Where(u => u.Entry == MobIdSerpent && !u.IsDead && u.Distance < 10000).OrderBy(u => u.Distance).ToList();
+            }
+        }
+
+        public bool IsQuestComplete()
+        {
+            var quest = StyxWoW.Me.QuestLog.GetQuestById((uint)QuestId);
+            return quest == null || quest.IsCompleted;
+        }
+		
+        private bool IsObjectiveComplete(int objectiveId, uint questId)
+        {
+            if (Me.QuestLog.GetQuestById(questId) == null)
+            {
+                return false;
+            }
+            int returnVal = Lua.GetReturnVal<int>("return GetQuestLogIndexByID(" + questId + ")", 0);
+            return
+                Lua.GetReturnVal<bool>(
+                    string.Concat(new object[] { "return GetQuestLogLeaderBoard(", objectiveId, ",", returnVal, ")" }), 2);
+        }
+
+        public Composite DoneYet
+        {
+            get
+            {
+                return
+                    new Decorator(ret => IsObjectiveComplete(1, (uint)QuestId), new Action(delegate
+                    {
+                        TreeRoot.StatusText = "Finished!";
+                        _isBehaviorDone = true;
+                        return RunStatus.Success;
+                    }));
+            }
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return _root ?? (_root = new Decorator(ret => !_isBehaviorDone, new PrioritySelector(
+			DoneYet,
+
+			new DecoratorContinue(ret => !IsObjectiveComplete(1, (uint)QuestId),
+                            new Sequence(  
+				new DecoratorContinue(ret => Serpent[0].Location.Distance(Me.Location) > 30,
+			     	new Sequence(
+                    			new Action(ret => Navigator.MoveTo(Serpent[0].Location)),
+		    			new Action(r => Serpent[0].Face())
+					)),
+				new DecoratorContinue(ret => Serpent[0].Location.Distance(Me.Location) <= 30,
+			     	new Sequence(
+                    			new Action(r => WoWMovement.MoveStop()),
+		    			new Action(r => Serpent[0].Face()),
+                    			SpellManager.Cast(SpellId);
+					)))),
+				new ActionAlwaysSucceed())));
+        }
+    }
+}
