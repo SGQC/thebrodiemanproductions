c36a0fdcf13317d3302d9677bbe68404bc026b30
brodiebruce08@gmail.com <brodiebruce08@gmail.com@72654b22-5f91-dfbe-c595-3f911aafe2ed>
2013-06-07 20:43:47 +0000

    r315 August Celestials -- Chasing Hope - Converted to use hotspots and mounting support Golden Pagoda Phase 2 -- Burning Away the Filth - Removed While loop Quest Behaviors -- Updates from CJ

--- trunk/Quest Behaviors/InteractWith.cs
+++ trunk/Quest Behaviors/InteractWith.cs
@@ -585,8 +585,8 @@ namespace Honorbuddy.Quest_Behaviors.InteractWith
         private WaitTimer _timerToReachDestination = null;
 
         // DON'T EDIT THESE--they are auto-populated by Subversion
-        public override string SubversionId { get { return ("$Id: InteractWith.cs 542 2013-06-04 19:46:55Z chinajade $"); } }
-        public override string SubversionRevision { get { return ("$Revision: 542 $"); } }
+        public override string SubversionId { get { return ("$Id: InteractWith.cs 549 2013-06-07 16:39:06Z chinajade $"); } }
+        public override string SubversionRevision { get { return ("$Revision: 549 $"); } }
         #endregion
 
 
@@ -984,11 +984,13 @@ namespace Honorbuddy.Quest_Behaviors.InteractWith
 
                             UtilityBehaviorPS_MoveTo(
                                 context => SelectedInteractTarget.Location,
-                                context => string.Format("interact with {0} (id: {1}, dist: {2:F1}{3})",
+                                context => string.Format("interact with {0} (id: {1}, dist: {2:F1}{3}, TtB: {4})",
                                                         GetName(SelectedInteractTarget),
                                                         SelectedInteractTarget.Entry,
                                                         SelectedInteractTarget.Distance,
-                                                        IsInLineOfSight(SelectedInteractTarget) ? "" : ", noLoS"))
+                                                        (IsInLineOfSight(SelectedInteractTarget) ? "" : ", noLoS"),
+                                                        // Time-to-Blacklist
+                                                        PrettyTime(_timerToReachDestination.TimeLeft)))
                         )),
 
                     // If we expect to gossip, and mob in combat and offers no gossip, help mob...
@@ -1341,11 +1343,7 @@ namespace Honorbuddy.Quest_Behaviors.InteractWith
                 if (QuestFrame.Instance.IsVisible)
                     { QuestFrame.Instance.Close(); }
                 if (TaxiFrame.Instance.IsVisible)
-                {
-                    // HBCORE BUG: TaxiFrame does not have a close method
-                    // new Action(context => { TaxiFrame.Instance.Close(); }),
-                    LogError("Unable to close Taxi Frame");
-                }
+                    { TaxiFrame.Instance.Close(); }
                 if (TrainerFrame.Instance.IsVisible)
                     { TrainerFrame.Instance.Close(); }
             }
